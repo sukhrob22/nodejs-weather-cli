86f9d642a2880aa487d26ca4272bde78c02efd72
sukhrob22 <ergashevsuxrob986@gmail.com>
2023-12-03 12:33:54 +0500

    Xabarni chop etish

--- helpers/args.js
+++ helpers/args.js
@@ -14,5 +14,4 @@ const getArgs = (args) => {
     });
     return res;
 };
-
-module.exports = getArgs;
+export default getArgs;
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const getArgs = require('./helpers/args');
+import getArgs from './helpers/args.js';
+import { printError, printSuccess, printHelp } from './services/log.service.js';
 
 const startCLI = () => {
     const args = getArgs(process.argv);
-
     if (args.h) {
-        //help
+        printHelp();
     }
     if (args.s) {
         //save city
