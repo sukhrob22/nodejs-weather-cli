cff6bcb5e10fc0e2f19e725355c2831b362aa6f8
sukhrob22 <ergashevsuxrob986@gmail.com>
2023-12-04 11:24:45 +0500

    Token saqalash

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 import getArgs from './helpers/args.js';
 import { printError, printSuccess, printHelp } from './services/log.service.js';
+import { saveKeyValue } from './services/storage.service.js';
+
+const saveToken = async (token) => {
+    try {
+        await saveKeyValue('token', token);
+        printSuccess('Token was saved');
+    } catch (error) {
+        printError(error.message);
+    }
+};
 
 const startCLI = () => {
     const args = getArgs(process.argv);
+    console.log(args);
     if (args.h) {
         printHelp();
     }
@@ -10,6 +21,7 @@ const startCLI = () => {
         //save city
     }
     if (args.t) {
+        return saveToken(args.t);
         //save token
     }
     //result
