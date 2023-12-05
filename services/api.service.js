08febbc1cf9f2aed7f3a24a63398ccc93b9cf509
sukhrob22 <ergashevsuxrob986@gmail.com>
2023-12-05 09:19:17 +0500

    Fix bug & AXIOS

--- services/api.service.js
+++ services/api.service.js
@@ -1,30 +1,43 @@
-import https from 'https';
+// import https from 'https';
+import axios from 'axios';
 import { TOKEN_DICTIONARY, getKeyValue } from './storage.service.js';
 
 const getWeather = async (city) => {
-    //https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-
-    const token = getKeyValue(TOKEN_DICTIONARY.token);
+    const token = await getKeyValue(TOKEN_DICTIONARY.token);
     if (!token) {
         throw new Error("Api doesn't exist, -t [API_KEY] for saving token");
     }
 
-    const url = new URL('https://api.openweathermap.org/data/2.5/weather');
-    url.searchParams.append('q', city);
-    url.searchParams.append('appid', token);
-    url.searchParams.append('lang', 'en');
-    url.searchParams.append('units', 'metric');
+    const response = await axios.get(
+        'https://api.openweathermap.org/data/2.5/weather',
+        {
+            params: {
+                q: city,
+                appid: token,
+                leng: 'en',
+                units: 'metric',
+            },
+        }
+    );
 
-    https.get(url, (response) => {
-        let res = '';
-        response.on('data', (chunk) => {
-            res += chunk;
-        });
+    return response.data;
 
-        response.on('end', () => {
-            console.log(res);
-        });
-    });
+    // const url = new URL('https://api.openweathermap.org/data/2.5/weather');
+    // url.searchParams.append('q', city);
+    // url.searchParams.append('appid', token);
+    // url.searchParams.append('lang', 'en');
+    // url.searchParams.append('units', 'metric');
+
+    // https.get(url, (response) => {
+    //     let res = '';
+    //     response.on('data', (chunk) => {
+    //         res += chunk;
+    //     });
+
+    //     response.on('end', () => {
+    //         console.log(res);
+    //     });
+    // });
 };
 
 export { getWeather };
